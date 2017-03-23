d78adea32a69ce077a1f2076bd9c0db614b9a07e
Tiago Bastos <tiagoalexbastos@gmail.com>
2017-03-23 00:28:00 +0000

    first progress lol :D

---
+++ doorbell-master/app/src/main/java/com/example/androidthings/doorbell/DoorbellActivity.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2016, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.androidthings.doorbell;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Base64;
+import android.util.Log;
+
+import com.google.android.things.contrib.driver.button.Button;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Map;
+
+/**
+ * Doorbell activity that capture a picture from the Raspberry Pi 3
+ * Camera on a button press and post it to Firebase and Google Cloud
+ * Vision API.
+ */
+public class DoorbellActivity extends Activity {
+    private static final String TAG = DoorbellActivity.class.getSimpleName();
+
+    private FirebaseDatabase mDatabase;
+    private DoorbellCamera mCamera;
+
+    /*
+     * Driver for the doorbell button;
+     */
+    private Button mButton;
+
+    /**
+     * A {@link Handler} for running Camera tasks in the background.
+     */
+    private Handler mCameraHandler;
+
+    /**
+     * An additional thread for running Camera tasks that shouldn't block the UI.
+     */
+    private HandlerThread mCameraThread;
+
+    /**
+     * A {@link Handler} for running Cloud tasks in the background.
+     */
+    private Handler mCloudHandler;
+
+    /**
+     * An additional thread for running Cloud tasks that shouldn't block the UI.
+     */
+    private HandlerThread mCloudThread;
+
+    /**
+     * The GPIO pin to activate to listen for button presses.
+     */
+    private final String BUTTON_GPIO_PIN = "BCM21";
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "Doorbell Activity created.");
+
+        // We need permission to access the camera
+        if (checkSelfPermission(Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            // A problem occurred auto-granting the permission
+            Log.d(TAG, "No permission");
+            return;
+        }
+
+        mDatabase = FirebaseDatabase.getInstance();
+
+        // Creates new handlers and associated threads for camera and networking operations.
+        mCameraThread = new HandlerThread("CameraBackground");
+        mCameraThread.start();
+        mCameraHandler = new Handler(mCameraThread.getLooper());
+
+        mCloudThread = new HandlerThread("CloudThread");
+        mCloudThread.start();
+        mCloudHandler = new Handler(mCloudThread.getLooper());
+
+        // Initialize the doorbell button driver
+        try {
+            mButton = new Button(BUTTON_GPIO_PIN, Button.LogicState.PRESSED_WHEN_LOW);
+            mButton.setOnButtonEventListener(mButtonCallback);
+        } catch (IOException e) {
+            Log.e(TAG, "button driver error", e);
+        }
+        // Camera code is complicated, so we've shoved it all in this closet class for you.
+        mCamera = DoorbellCamera.getInstance();
+        mCamera.initializeCamera(this, mCameraHandler, mOnImageAvailableListener);
+    }
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mCamera.shutDown();
+
+        mCameraThread.quitSafely();
+        mCloudThread.quitSafely();
+        try {
+            mButton.close();
+        } catch (IOException e) {
+            Log.e(TAG, "button driver error", e);
+        }
+    }
+
+    /**
+     * Callback for button events.
+     */
+    private Button.OnButtonEventListener mButtonCallback = new Button.OnButtonEventListener() {
+        @Override
+
+        public void onButtonEvent(Button button, boolean pressed) {
+            if (pressed) {
+                // Doorbell rang!
+                Log.d(TAG, "button pressed");
+                mCamera.takePicture();
+            }
+        }
+    };
+
+    /**
+     * Listener for new camera images.
+     */
+    private ImageReader.OnImageAvailableListener mOnImageAvailableListener =
+            new ImageReader.OnImageAvailableListener() {
+        @Override
+        public void onImageAvailable(ImageReader reader) {
+            Image image = reader.acquireLatestImage();
+            // get image bytes
+            ByteBuffer imageBuf = image.getPlanes()[0].getBuffer();
+            final byte[] imageBytes = new byte[imageBuf.remaining()];
+            imageBuf.get(imageBytes);
+            image.close();
+
+            onPictureTaken(imageBytes);
+        }
+    };
+
+    /**
+     * Handle image processing in Firebase and Cloud Vision.
+     */
+    private void onPictureTaken(final byte[] imageBytes) {
+        if (imageBytes != null) {
+            final DatabaseReference log = mDatabase.getReference("imagens") ;
+            String imageStr = Base64.encodeToString(imageBytes, Base64.NO_WRAP | Base64.URL_SAFE);
+            // upload image to firebase
+            log.child("imagem").setValue(imageStr);
+            Log.i(TAG, String.valueOf(imageBytes));
+        }
+    }
+}
